4c164015cd728acc73df853cb6eb287d6ec0d579
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-29 10:42:24 +0200

    getPokemons()

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,4 +11,12 @@ export class DataService {
   constructor(
     private http: HttpClient
     ) { }
+
+    // Get Pokemons from API
+    getPokemons(){
+      // conenct to the api requesting just 10 pokemons
+      // that is how this api is built
+      return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=10`)
+    }
+
 }
