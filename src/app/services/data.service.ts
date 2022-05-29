b2e77b18c32d24ded29c6b4d233091c01dbc6398
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-29 10:39:37 +0200

    import HttpClient into service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
+// Import HttpClient and add it to constructor
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+    ) { }
 }
